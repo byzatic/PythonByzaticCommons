2152435170a6dddbc51ef3c7bd20238491f57b4c
Svyatoslav Vlasov <s.vlasov.home@icloud.com>
2025-08-01 11:02:00 +0300

    bugfix: develop...

--- python_byzatic_commons/logging_manager/LoggingManager.py
+++ python_byzatic_commons/logging_manager/LoggingManager.py
@@ -7,6 +7,7 @@
 # ERROR - Due to a more serious problem, the software has not been able to perform some function.
 # CRITICAL - A serious error, indicating that the program itself may be unable to continue running.
 #
+import os
 import sys
 import logging
 from logging.config import dictConfig, fileConfig
@@ -17,12 +18,13 @@ from python_byzatic_commons.logging_manager.interfaces.LoggingManagerInterface i
 
 
 class LoggingManager(LoggingManagerInterface):
-    def __init__(self):
+    def __init__(self, base_path: str = __file__):
         self.__configuration = dict()
         try:
             self.logger = logging.getLogger("root")
             self.__json_reader = JsonFileReader()
             self.__yaml_reader = YamlFileReader()
+            self.__base_path = base_path
         except OperationIncompleteException as oie:
             raise OperationIncompleteException(oie.args, errno=oie.errno)
         except Exception as err:
@@ -62,6 +64,7 @@ class LoggingManager(LoggingManagerInterface):
     def __setup_dict(self, configuration) -> None:
         try:
             if configuration is not None:
+                self.__create_log_dirs_from_handlers(configuration)
                 dictConfig(configuration)
             else:
                 raise OperationIncompleteException(f"Dictionary logger configuration is not set")
@@ -70,6 +73,26 @@ class LoggingManager(LoggingManagerInterface):
             raise OperationIncompleteException(oie.args, errno=oie.errno)
         except Exception as err:
             raise OperationIncompleteException(err.args)
+
+    def __create_log_dirs_from_handlers(self, logging_config: dict) -> None:
+        """
+        Create directories for file-based handlers in a logging config.
+        If a filename is relative, it is resolved against base_path.
+
+        :param logging_config: dict-style logging configuration
+        :param base_path: base directory to resolve relative filenames
+        """
+        handlers = logging_config.get("handlers", {})
+        for handler_name, handler in handlers.items():
+            filename = handler.get("filename")
+            if filename:
+                if not os.path.isabs(filename):
+                    filename = os.path.join(self.__base_path, filename)
+                    handler["filename"] = filename
+
+                directory = os.path.dirname(filename)
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
 
     # Configure __logger
     def init_logging(self, configuration_file: str, configuration_type: str, configuration_dict: dict = None) -> None:
