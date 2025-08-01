2369364957731160af2f6daf65a07a370e6f75c8
Svyatoslav Vlasov <s.vlasov.home@icloud.com>
2025-08-01 08:52:00 +0300

    bugfix: develop...

--- python_byzatic_commons/__init__.py
+++ python_byzatic_commons/__init__.py
@@ -11,7 +11,7 @@ import python_byzatic_commons.logging_manager
 import python_byzatic_commons.random_string_generator
 import python_byzatic_commons.singleton
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     'crud',
     'exceptions',
